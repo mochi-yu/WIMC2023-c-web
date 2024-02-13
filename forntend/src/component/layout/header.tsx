4db0cec1f24a74569d50c35e6ef53130b026f4a3
e28992 <aice.11037.banira@gmail.com>
2024-02-13 22:02:58 +0900

    wip

---
+++ forntend/src/component/layout/header.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Stack, Typography, IconButton, createTheme, ThemeProvider } from '@mui/material';
+
+export function Header() {
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      spacing={2}
+      sx={{ px: '20px', bgcolor: '#006B5D' }}
+      height="80px"
+    >
+      <Typography fontSize={32} color="white">
+        スキー記録  
+      </Typography>
+      <IconButton />
+    </Stack>
+  );
+}
---
+++ forntend/src/component/layout/load.tsx
@@ -0,0 +1,12 @@
+import { Backdrop, CircularProgress } from '@mui/material';
+
+//ロード
+export function Loading() {
+  return (
+    <>
+      <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
+  );
+}
