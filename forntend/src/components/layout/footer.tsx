d53a405fad4f136026169e86ccc4672af3e0b20d
mochi-yu <mochi.2225.it@gmail.com>
2024-02-27 21:36:59 +0900

    フッターのフォーマット

--- forntend/src/components/layout/footer.tsx
+++ forntend/src/components/layout/footer.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   BottomNavigation,
@@ -9,36 +9,35 @@ import {
   Stack,
   Typography,
   colors,
-} from '@mui/material';
-import { Home, Assignment, Flag, Style } from '@mui/icons-material';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
+} from "@mui/material";
+import { Home, Assignment, Flag, Style } from "@mui/icons-material";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export function Footer() {
   const pathname = usePathname();
 
   const footerButtonsList = [
-    { label: 'トップ', icon: <Home sx={{color: 'white'}} />, link_to: '/home', },
-    { label: '記録一覧', icon: <Assignment sx={{color: 'white'}} />, link_to: '/record' },
-    { label: '目標設定', icon: <Flag sx={{color: 'white'}} />, link_to: '/Target' },
+    { label: "トップ", icon: <Home sx={{ color: "white" }} />, link_to: "/home" },
+    { label: "記録一覧", icon: <Assignment sx={{ color: "white" }} />, link_to: "/record" },
+    { label: "目標設定", icon: <Flag sx={{ color: "white" }} />, link_to: "/target" },
   ];
 
   const footerButtons = footerButtonsList.map((elm) => {
     function setMode(arg0: string) {
-      throw new Error('Function not implemented.');
+      throw new Error("Function not implemented.");
     }
 
     return (
-      
       <BottomNavigationAction
         sx={{
           color: "#202f55",
-          ":hover": {color: "white"}
+          ":hover": { color: "white" },
         }}
         key={elm.label}
         value={elm.link_to}
         label={
-          <Typography fontSize={14} marginX={'-10px'} sx={{color: 'white'}}>
+          <Typography fontSize={14} marginX={"-10px"} sx={{ color: "white" }}>
             {elm.label}
           </Typography>
         }
@@ -51,14 +50,14 @@ export function Footer() {
 
   return (
     <Box sx={{ pb: 10 }}>
-      <Stack justifyContent="center" alignItems="center">
-        <Container sx={{ position: 'fixed', bottom: 0, px: 0, zIndex: 5 }} maxWidth="xs">
+      <Stack justifyContent='center' alignItems='center'>
+        <Container sx={{ position: "fixed", bottom: 0, px: 0, zIndex: 5 }} maxWidth='xs'>
           <Paper sx={{ px: 0 }}>
             <BottomNavigation
               showLabels
               value={pathname}
               children={footerButtons}
-              sx={{ bgcolor: '#4496d3' }}
+              sx={{ bgcolor: "#4496d3" }}
             />
           </Paper>
         </Container>
