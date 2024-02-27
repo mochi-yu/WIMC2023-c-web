fcac71a904c72dd5fec552dc0bfe717c0a9656bf
mochi-yu <mochi.2225.it@gmail.com>
2024-02-27 16:36:01 +0900

    feat: `/`ページを追加

--- forntend/src/app/(not_login)/page.tsx
+++ forntend/src/app/(not_login)/page.tsx
@@ -1,9 +1,16 @@
-import { Typography } from "@mui/material";
+import { GoogleLoginButton } from "@/components/ui/button/login_google";
+import { Stack, Typography } from "@mui/material";
 
 export default function RootPage() {
   return (
     <>
-      <Typography>Root</Typography>
+      <Stack justifyContent='center' alignItems='center' height='100vh' spacing='50px'>
+        <Stack alignItems='center'>
+          <Typography variant='h5'>Web IoT メイカーズチャレンジ</Typography>
+          <Typography variant='h5'>信州Cチーム</Typography>
+        </Stack>
+        <GoogleLoginButton />
+      </Stack>
     </>
-  )
+  );
 }
