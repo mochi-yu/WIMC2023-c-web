8f46dc73ff4f0cf8046d43268e7f1333760fa614
ice28992 <ice28992@gmail.com>
2024-03-01 00:01:15 +0900

    fix: mapのピンによるエラー修正

--- forntend/src/app/(login)/home/map.tsx
+++ forntend/src/app/(login)/home/map.tsx
@@ -3,17 +3,12 @@
 import { Box } from "@mui/material";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { icon } from "leaflet";
 
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon.src,
-  iconRetinaUrl: markerIcon2x.src,
-  shadowUrl: markerShadow.src,
-});
+const ICON = icon({
+  iconUrl: "https://esm.sh/leaflet@1.9.2/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+})
 
 const Data = {
   placeId: 0,
@@ -49,7 +44,7 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[setLat, setLon]}>
+      <Marker icon={ICON} position={[setLat, setLon]}>
         <Popup>
           {Data.placeName}
         </Popup>
