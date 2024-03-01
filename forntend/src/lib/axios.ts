5ce9f636bc319744d9d1c3831fd30a20cfb6333f
mochi-yu <mochi.2225.it@gmail.com>
2024-03-02 01:23:46 +0900

    feat: axiosを追加

---
+++ forntend/src/lib/axios.ts
@@ -0,0 +1,42 @@
+import { auth } from "@/lib/firebase";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export async function GetWithLogin(path: string): Promise<any> {
+  if (!auth.currentUser) return;
+
+  const idToken = await auth.currentUser.getIdToken();
+
+  return await instance
+    .get(path, {
+      headers: { Authorization: "Bearer " + idToken },
+    })
+    .then((res) => {
+      const { data } = res;
+      return data;
+    });
+}
+
+export async function PostWithLogin(path: string, data: any): Promise<any> {
+  if (!auth.currentUser) return;
+
+  const idToken = await auth.currentUser.getIdToken();
+
+  return await instance
+    .post(path, data, {
+      headers: { Authorization: "Bearer " + idToken },
+    })
+    .then((res) => {
+      const { data } = res;
+      return data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
