14e3c771d30e60e7bf8ab2a17a83c17659ceec31
mochi-yu <mochi.2225.it@gmail.com>
2024-03-02 02:25:39 +0900

    feat: ユーザ名を変更するモーダルを追加

--- forntend/src/app/(login)/home/_components/user_name.tsx
+++ forntend/src/app/(login)/home/_components/user_name.tsx
@@ -2,17 +2,30 @@
 
 import { UserInfoContext } from "@/components/contexts/auth";
 import { Button, Stack, Typography } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { UserNameChangeModal } from "./user_name_change_modal";
 
 export const UserName = () => {
   const { userInfo } = useContext(UserInfoContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
   const userName = userInfo?.userName || "";
   console.log(userInfo);
 
   return (
     <Stack sx={{ margin: "30px", marginBottom: "0px" }}>
       <Typography variant='h5'>{userName} さん</Typography>
-      <Button sx={{ width: "120px", alignItems: "left" }}>ユーザ名を変更</Button>
+      <Button sx={{ width: "120px", alignItems: "left" }} onClick={handleOpen}>
+        ユーザ名を変更
+      </Button>
+      <UserNameChangeModal isOpen={isOpen} handleClose={handleClose} userName={userName} />
     </Stack>
   );
 };
