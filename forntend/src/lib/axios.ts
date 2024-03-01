18be59a6e1d8b5b2ee7b88dfbe4e9651e1f5be62
mochi-yu <mochi.2225.it@gmail.com>
2024-03-02 02:24:03 +0900

    fix: PUT / PATCH / DELETE のメソッドを追加

--- forntend/src/lib/axios.ts
+++ forntend/src/lib/axios.ts
@@ -40,3 +40,54 @@ export async function PostWithLogin(path: string, data: any): Promise<any> {
       console.log(e);
     });
 }
+
+export async function PatchWithLogin(path: string, data: any): Promise<any> {
+  if (!auth.currentUser) return;
+
+  const idToken = await auth.currentUser.getIdToken();
+
+  return await instance
+    .patch(path, data, {
+      headers: { Authorization: "Bearer " + idToken },
+    })
+    .then((res) => {
+      const { data } = res;
+      return data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+export async function PutWithLogin(path: string, data: any): Promise<any> {
+  if (!auth.currentUser) return;
+
+  const idToken = await auth.currentUser.getIdToken();
+
+  return await instance
+    .put(path, data, {
+      headers: { Authorization: "Bearer " + idToken },
+    })
+    .then((res) => {
+      const { data } = res;
+      return data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+export async function DeleteWithLogin(path: string): Promise<any> {
+  if (!auth.currentUser) return;
+
+  const idToken = await auth.currentUser.getIdToken();
+
+  return await instance
+    .delete(path, {
+      headers: { Authorization: "Bearer " + idToken },
+    })
+    .then((res) => {
+      const { data } = res;
+      return data;
+    });
+}
