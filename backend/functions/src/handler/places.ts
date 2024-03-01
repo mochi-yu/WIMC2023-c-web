0de36e5df0e1a268be52838eac280b75a1fd7a4e
mochi-yu <mochi.2225.it@gmail.com>
2024-03-02 01:23:04 +0900

    fix: CORSの設定を記述

--- backend/functions/src/handler/places.ts
+++ backend/functions/src/handler/places.ts
@@ -3,21 +3,37 @@ import * as express from "express";
 export const placeRouter = express.Router();
 
 placeRouter.get("/places", async (req, res) => {
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+
   res.status(200).send("get /place");
   return;
 });
 
 placeRouter.post("/places", async (req, res) => {
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+
   res.status(201).send("post /place");
   return;
 });
 
 placeRouter.put("/places/:placeId", async (req, res) => {
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+
   res.status(201).send("put /places/:placeId");
   return;
 });
 
 placeRouter.delete("/places/:placeId", async (req, res) => {
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+
   res.status(201).send("delete /places/:placeId");
   return;
 });
--- backend/functions/src/handler/records.ts
+++ backend/functions/src/handler/records.ts
@@ -3,16 +3,28 @@ import * as express from "express";
 export const recordRouter = express.Router();
 
 recordRouter.get("/records", async (req, res) => {
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+
   res.status(200).send("record get");
   return;
 });
 
 recordRouter.post("/records", async (req, res) => {
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+
   res.status(201).send("record post");
   return;
 });
 
 recordRouter.patch("/records/:recordId/place", async (req, res) => {
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST");
+
   res.status(201).send("patch /records/:recordId/place");
   return;
 });
