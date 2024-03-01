663d28fa7a721508261a1dfb56f4c308ec636649
mochi-yu <mochi.2225.it@gmail.com>
2024-03-01 18:25:45 +0900

    fix: マージンを修正

--- forntend/src/app/(login)/home/page.tsx
+++ forntend/src/app/(login)/home/page.tsx
@@ -1,5 +1,5 @@
 import { RecentRecordCard } from "./recent_record_card";
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Divider, Stack, Typography } from "@mui/material";
 import Map from "./map";
 import { Zen_Maru_Gothic } from "next/font/google";
 
@@ -16,17 +16,18 @@ export default function HomePage() {
   const userName = "サンプルユーザー";
   return (
     <Stack className={zen.className} sx={{ backgroundColor: "#fff9f4" }}>
-      <Stack sx={{ margin: "30px" }}>
+      <Stack sx={{ margin: "30px", marginBottom: "0px" }}>
         <Typography variant='h5'>{userName} さん</Typography>
         <Button sx={{ width: "120px", alignItems: "left" }}>ユーザ名を変更</Button>
       </Stack>
 
-      <Stack sx={{ marginTop: 5 }}>
+      <Stack>
         <RecentRecordCard />
       </Stack>
       <br />
 
-      <line style={{ width: "100%", border: "double #4496d3 3px" }}></line>
+      <Divider style={{ width: "100%", border: "double #4496d3 3px" }} />
+
       <h2
         style={{
           textAlign: "center",
