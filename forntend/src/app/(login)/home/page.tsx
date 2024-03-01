2d3be3b5eb02fffb9b8912798f32a3ea51c5343b
mochi-yu <mochi.2225.it@gmail.com>
2024-03-01 17:56:27 +0900

    feat: フォーマット

--- forntend/src/app/(login)/home/page.tsx
+++ forntend/src/app/(login)/home/page.tsx
@@ -7,27 +7,33 @@ const zen = Zen_Maru_Gothic({
   weight: ["400"],
   subsets: ["latin"],
   display: "swap",
-})
+});
 
- {/*ホーム*/}
+{
+  /*ホーム*/
+}
 export default function HomePage() {
   return (
-    <Stack className={zen.className} sx={{ backgroundColor: "#fff9f4"}}>
-      <Stack sx={{marginTop: 5}}>
+    <Stack className={zen.className} sx={{ backgroundColor: "#fff9f4" }}>
+      <Stack sx={{ marginTop: 5 }}>
         <RecentRecordCard />
-      </Stack><br />
-      
-      <line style={{width: "100%", border: "double #4496d3 3px"}}></line>
-      <h2 style={{ 
-        textAlign: "center", 
-        margin: 30,
-        borderRadius: "10px",
-        backgroundColor: "#4496d3",
-        color: "white"
-      }}>
+      </Stack>
+      <br />
+
+      <line style={{ width: "100%", border: "double #4496d3 3px" }}></line>
+      <h2
+        style={{
+          textAlign: "center",
+          margin: 30,
+          borderRadius: "10px",
+          backgroundColor: "#4496d3",
+          color: "white",
+        }}
+      >
         スキーコース
       </h2>
-      <Map /><br />
+      <Map />
+      <br />
     </Stack>
   );
 }
