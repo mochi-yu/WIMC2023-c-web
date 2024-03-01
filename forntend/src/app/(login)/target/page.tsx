e7d8a8198f24306754a76f18667e64ba6602545d
mochi-yu <mochi.2225.it@gmail.com>
2024-03-01 18:19:34 +0900

    fix: フォーマットと、背景色の削除

--- forntend/src/app/(login)/target/page.tsx
+++ forntend/src/app/(login)/target/page.tsx
@@ -6,25 +6,29 @@ const zen = Zen_Maru_Gothic({
   weight: ["400"],
   subsets: ["latin"],
   display: "swap",
-})
+});
 
-{/*目標設定*/}
+{
+  /*目標設定*/
+}
 export default function TargetPage() {
   return (
-    <Stack className={zen.className} style={{ backgroundColor: "#fff9f4"}}>
-      <h1 style={{ 
-       background: "#68a9cf",
-       boxShadow: "0px 0px 0px 5px #68a9cf",
-       border: "dashed 2px white",
-       padding: "0.2em 0.5em", 
-       width: 300,
-       margin: "auto",
-       marginTop: "20px",
-       marginBottom: "20px",
-       textAlign: "center",
-       borderRadius: "10px",
-       color: "white"
-      }}>
+    <Stack className={zen.className}>
+      <h1
+        style={{
+          background: "#68a9cf",
+          boxShadow: "0px 0px 0px 5px #68a9cf",
+          border: "dashed 2px white",
+          padding: "0.2em 0.5em",
+          width: 300,
+          margin: "auto",
+          marginTop: "20px",
+          marginBottom: "20px",
+          textAlign: "center",
+          borderRadius: "10px",
+          color: "white",
+        }}
+      >
         目標設定
       </h1>
       <Targetcontent />
