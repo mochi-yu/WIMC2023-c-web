ac77ac7d0ffa275592d4dca0c4ed348094ed8c25
mochi-yu <mochi.2225.it@gmail.com>
2024-03-03 07:59:16 +0900

    feat: マップに地点のピンを表示

--- forntend/src/app/(login)/home/map.tsx
+++ forntend/src/app/(login)/home/map.tsx
@@ -4,53 +4,71 @@ import { Box } from "@mui/material";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { icon } from "leaflet";
+import { useEffect, useState } from "react";
+import { PlaceData } from "@/model/place";
+import { GetWithLogin } from "@/lib/axios";
+import Link from "next/link";
 
 const ICON = icon({
   iconUrl: "https://esm.sh/leaflet@1.9.2/dist/images/marker-icon.png",
   iconSize: [25, 41],
-})
-
-const Data = {
-  placeId: 0,
-  placeName: "白馬五竜",
-};
+});
 
 const Map = () => {
-  const setLat = 36.66324682492006;  //緯度
-  const setLon = 137.83681420357394; //経度
-  
-  return (
-    <Box sx={{
-      backgroundColor: "#b2cbe4", 
-      m:"30px", 
-      borderRadius: "6px",
-      boxShadow: "2px 2px 5px rgba(0,0,0,0.5)",
-    }}>
-    <MapContainer
-      center={[setLat, setLon]}
-      zoom={16}
-      scrollWheelZoom={false}
-      style={{ 
-        margin: "auto", 
-        marginBottom: 10,
-        marginTop: 10,
-        padding:10,
-        height: "300px", 
-        width: "300px", 
-        borderRadius: "6px",
-        zIndex: 0
-      }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker icon={ICON} position={[setLat, setLon]}>
+  const initLat = 36.11128719095136;
+  const initLon = 137.95052602342327;
+  const [places, setPlaces] = useState<PlaceData[]>([]);
+
+  useEffect(() => {
+    getPlaceList();
+  }, []);
+
+  const getPlaceList = async () => {
+    const places = (await GetWithLogin("/places"))["places"];
+    setPlaces(places);
+    return;
+  };
+
+  const placeMarker = places.map((elm) => {
+    return (
+      <Marker icon={ICON} position={[elm.lat, elm.lon]} key={elm.placeId}>
         <Popup>
-          {Data.placeName}
+          <Link href={"/places?placeId=" + elm.placeId}>{elm.placeName}</Link>
         </Popup>
       </Marker>
-    </MapContainer>
+    );
+  });
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#b2cbe4",
+        m: "30px",
+        borderRadius: "6px",
+        boxShadow: "2px 2px 5px rgba(0,0,0,0.5)",
+      }}
+    >
+      <MapContainer
+        center={[initLat, initLon]}
+        zoom={8}
+        scrollWheelZoom={true}
+        style={{
+          margin: "auto",
+          marginBottom: 10,
+          marginTop: 10,
+          padding: 10,
+          height: "300px",
+          width: "300px",
+          borderRadius: "6px",
+          zIndex: 0,
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        {placeMarker}
+      </MapContainer>
     </Box>
   );
 };
